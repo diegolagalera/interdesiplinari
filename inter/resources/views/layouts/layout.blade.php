274eef8ea0ef3316215b1736e7623e656e16a50e
diegolagalera <diegomartinezmartinez@iesmontsia.org>
2018-05-04 15:26:31 +0200

    QWEWQE

--- inter/resources/views/layouts/layout.blade.php
+++ inter/resources/views/layouts/layout.blade.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png')}}">
+	<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/img/favicon.png')}}">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<title>Paper Dashboard by Creative Tim</title>
@@ -19,17 +19,17 @@
     <link href="{{ asset('assets/css/animate.min.css') }}" rel="stylesheet"/>
 
     <!--  Paper Dashboard core CSS    -->
-    <link href="assets/css/paper-dashboard.css" rel="stylesheet"/>
+    <link href="{{ asset('assets/css/paper-dashboard.css') }}" rel="stylesheet"/>
 
 
     <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="assets/css/demo.css" rel="stylesheet" />
+    <link href="{{ asset('assets/css/demo.css')}}" rel="stylesheet" />
 
 
     <!--  Fonts and icons     -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Muli:400,300' rel='stylesheet' type='text/css'>
-    <link href="assets/css/themify-icons.css" rel="stylesheet">
+    <link href="{{ asset('assets/css/themify-icons.css')}}" rel="stylesheet">
 
 </head>
 <body>
@@ -203,26 +203,26 @@
 </body>
 
     <!--   Core JS Files   -->
-    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
-	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="{{ asset('assets/js/jquery-1.10.2.js')}}" type="text/javascript"></script>
+	<script src="{{ asset('assets/js/bootstrap.min.js')}}" type="text/javascript"></script>
 
 	<!--  Checkbox, Radio & Switch Plugins -->
-	<script src="assets/js/bootstrap-checkbox-radio.js"></script>
+	<script src="{{ asset('assets/js/bootstrap-checkbox-radio.js')}}"></script>
 
 	<!--  Charts Plugin -->
-	<script src="assets/js/chartist.min.js"></script>
+	<script src="{{ asset('assets/js/chartist.min.js')}}"></script>
 
     <!--  Notifications Plugin    -->
-    <script src="assets/js/bootstrap-notify.js"></script>
+    <script src="{{ asset('assets/js/bootstrap-notify.js')}}"></script>
 
     <!--  Google Maps Plugin    -->
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 
     <!-- Paper Dashboard Core javascript and methods for Demo purpose -->
-	<script src="assets/js/paper-dashboard.js"></script>
+	<script src="{{ asset('assets/js/paper-dashboard.js')}}"></script>
 
 	<!-- Paper Dashboard DEMO methods, don't include it in your project! -->
-	<script src="assets/js/demo.js"></script>
+	<script src="{{ asset('assets/js/demo.js')}}"></script>
 
 	<script type="text/javascript">
     	$(document).ready(function(){
