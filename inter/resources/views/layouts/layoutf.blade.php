7a6735894b22b48d10fc8b6d07b44df0cc508e5c
ssjordiss <34072263+ssjordiss@users.noreply.github.com>
2018-05-05 00:21:27 +0200

    verygood pataques en zumo

--- inter/resources/views/layouts/layoutf.blade.php
+++ inter/resources/views/layouts/layoutf.blade.php
@@ -41,7 +41,7 @@
                       </a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="{{ url('create') }}">
+                      <a class="nav-link" href="{{ route('register') }}">
                           <i class="material-icons"></i> {{ __('Register') }}
                       </a>
                   </li>
