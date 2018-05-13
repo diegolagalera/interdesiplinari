a91d567a79a51d4c6336950c6b21039866d5e171
diegolagalera <diegomartinezmartinez@iesmontsia.org>
2018-05-13 21:15:53 +0200

    asd

--- inter/app/Http/Controllers/HomeController.php
+++ inter/app/Http/Controllers/HomeController.php
@@ -11,10 +11,9 @@ use Mail;
 
 class HomeController extends Controller
 {
-  public function __construct() {
-      $this->middleware(['auth', 'clearance'])->except(['index','mapa','contactar']);
-  }
-
+    public function __construct() {
+        $this->middleware(['auth', 'clearance'])->except(['index','mapa','contactar']);
+    }
     /**
      * Show the application dashboard.
      *
