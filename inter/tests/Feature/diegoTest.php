b0a8dedb9c9d4aa2fd9661402a3e0076bb7c5d11
diegolagalera <34072134+diegolagalera@users.noreply.github.com>
2018-05-16 14:59:42 +0200

    asd
    asd

--- inter/tests/Feature/diegoTest.php
+++ inter/tests/Feature/diegoTest.php
@@ -1,87 +1,20 @@
 <?php
 
-// namespace Tests\Feature;
+namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use App\User;
-class diegoTest extends TestCase
+
+class diegotest extends TestCase
 {
-  use WithoutMiddleware;
     /**
      * A basic test example.
      *
      * @return void
      */
-     // public function testBasicTest()
-     //     {
-     //         $response = $this->get('/users');
-     //         $response->assertStatus(200);
-     //     }
-
-    //  public function test_basic()
-    // {
-    //   $this-visit('/');
-    //
-    //
-
-     public function test_basic()
+    public function testExample()
     {
-      $response=$this->get('/');
-      $response->assertStatus(200);
-
+        $this->assertTrue(true);
     }
-     public function testApplication()
-    {
-        $user = factory( User::class)->create();
-
-        $response = $this->actingAs($user)
-                         ->withSession(['foo' => 'bar'])
-                         ->get('/productos');
-                         self::assertTrue(true);
-    }
-
-
-    //
-    // public function testHasItemInUsers()
-    //     {
-    //         $box = new User([
-    //           'name' => 'yish',
-    //           'cognom'=> 'asd',
-    //           'email_confirm'=>1,
-    //           'dni'=> '47384333',
-    //           'tel'=> '9081723',
-    //           'localitat'=> 'asdasd',
-    //           'email'=> 'diegolagalera_@hotmail.com',
-    //           'password'=> '123456'
-    //         ]);
-    //
-    //         $this->be($box);
-    //
-    //         $this->assertTrue($user->name=('John'));
-    //         $this->assertFalse($box->has('ball'));
-    //     }
-     // public function test_usuarios()
-     // {
-     //   $user=factory(App\User::class)->make([
-     //       'name'=> 'asd',
-     //       'cognom'=> 'asd',
-     //       'email_confirm'=> 1,
-     //       'dni'=> '47484339b',
-     //       'tel'=> '977719234',
-     //       'localitat'=> 'galera',
-     //       'email'=> 'asd@hotmail.com',
-     //       'password'=> '123456',
-     //     ]);
-     //
-     //   $this->actingAs($user)
-     //   ->withSession(['foo'=>'bar'])
-     //   ->visit('/')
-     //   ->see('ofertas');
-     //
-     // }
 }
